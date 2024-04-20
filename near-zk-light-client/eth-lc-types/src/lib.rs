d79237c99f02d87ca8ad3d0837dbee14fa82c9ee
suraj <surajj3404@gmail.com>
2024-04-21 02:48:51 +0530

    add eth-types

---
+++ near-zk-light-client/eth-lc-types/src/lib.rs
@@ -0,0 +1,17 @@
+
+
+// pub struct BeaconBlockHeader {
+//     pub slot: u64,
+//     pub parent_root: Hash256,
+//     pub state_root: Hash256,
+//     pub body_root: Hash256,
+//     pub proposer_index: Signature
+// }
+// pub struct LCHeader {
+//     pub beacon: BeaconBlockHeader
+// }
+
+pub fn verify_update() {
+
+}
+
